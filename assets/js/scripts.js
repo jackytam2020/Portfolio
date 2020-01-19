396483e9e0f5eb3066956ec0c6e2a5cf032ce85a
unknown <jackytam2020@gmail.com>
2020-01-18 16:46:40 -0800

    completely restructured layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,23 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $('.materialboxed').materialbox();
+    $(".dropdown-trigger").dropdown();
+    $(".dropdown-content>li>a").css("color", themeColor);
   });
-
  
+ 
+
+ var clipboard = new ClipboardJS('#gmail-logo');
+
+ clipboard.on('success', function(e) {
+     toast.style.display = "flex"
+     console.info('Text:', e.text);
+     console.info('Trigger:', e.trigger);
+ 
+     e.clearSelection();
+ });
+ 
+ clipboard.on('error', function(e) {
+     console.error('Action:', e.action);
+     console.error('Trigger:', e.trigger);
+ });
